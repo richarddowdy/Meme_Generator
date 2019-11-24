dc711626c4485f0a2e23d388b70cfcb5803cab75
Richard Dowdy <richard.dowdy0@gmail.com>
2019-11-24 18:12:11 -0500

    meme generator appears to be working properly

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 window.onload = function() {
-    let form, topText, bottomText, imgUrl, genMeme, gallery
+    // Declaring variables
+    let form, topText, bottomText, imgUrl, gallery
 
     topText = document.querySelector(".top-text");
     bottomText = document.querySelector(".bottom-text");
     imgUrl = document.querySelector(".image-url");
     gallery = document.querySelector(".gallery");
     form = document.getElementById("form");
-    //genMeme = document.querySelector(".gen-meme");
 
     form.addEventListener("submit", function(){
-        //creating all the internal meme elements
+        // Creating all the internal meme elements
         let newMeme = document.createElement("div");
         let topMeme = document.createElement("div");
         let bottomMeme = document.createElement("div");
@@ -17,73 +17,70 @@ window.onload = function() {
         
         //checking the length of the meme text top&bottom
         if(topText.value.length > 20 || bottomText.value.length > 20){
+            // Alerts the user that the text is too long
             alert("Your text is too long\n Please use less than 20 characters per line");
+            event.preventDefault();
+
+            //this can be broken up into more elif statements in order to be 
+            //more specific about which, or both, text fields is too long.
         } else {
             console.log(topText.value);
             topMeme.innerHTML = topText.value
             bottomMeme.innerHTML = bottomText.value
+        
+        
+            // Adding a newMeme div to the dom with top&bottom text divs inside
+            gallery.appendChild(newMeme);
+            memeImg.src = imgUrl.value;
+            newMeme.appendChild(memeImg);
+            newMeme.appendChild(topMeme);
+            newMeme.appendChild(bottomMeme);
+            
+
+            
+            // New Meme styling
+            newMeme.style.height = "auto";
+            newMeme.style.display = "inline-block";
+            newMeme.style.width = "40%"
+            newMeme.style.margin = "40px";
+            newMeme.style.position = "relative";
+            
+            // Image styling
+            memeImg.style.width = "100%";
+            memeImg.style.height = "auto";
+
+            // Meme text styling
+            //top
+            topMeme.style.position = "absolute";
+            topMeme.style.marginBottom = "40px";
+            topMeme.style.color = "white"
+            topMeme.style.textAlign = "center";
+            topMeme.style.fontSize = "30px";
+            //bottom
+            bottomMeme.style.position = "absolute";
+            bottomMeme.style.marginTop = "40px";
+            bottomMeme.style.color = "white";
+            bottomMeme.style.textAlign = "center";
+            bottomMeme.style.fontSize = "30px";
+
+            // Centers the meme 
+            //top
+            topMeme.style.top = "15%";
+            topMeme.style.left = "50%";
+            topMeme.style.zIndex = "1";
+            topMeme.style.transform = "translate(-50%, -50%)";
+            //bottom
+            bottomMeme.style.bottom = "5%";
+            bottomMeme.style.left = "50%";
+            bottomMeme.style.zIndex = "1";
+            bottomMeme.style.transform = "translate(-50%, -50%)";
+            
+            // Prevent refresh after function executes
+            event.preventDefault();
         }
-        
-        //adding a newMeme div to the dom with top&bottom text inside
-        gallery.appendChild(newMeme);
-        memeImg.src = imgUrl.value;
-        newMeme.appendChild(memeImg);
-        newMeme.appendChild(topMeme);
-        newMeme.appendChild(bottomMeme);
-        
-
-        //newMeme and image styling
-        memeImg.style.width = "100%";
-        memeImg.style.height = "auto";
-        //memeImg.style.position = "relative";
-        newMeme.style.height = "auto";
-        newMeme.style.display = "inline-block";
-        newMeme.style.width = "30%"
-        newMeme.style.margin = "40px";
-        newMeme.style.position = "relative";
-        
-
-        //newmeme text styling
-        // topMeme.style.display = "inline-block";
-        // bottomMeme.style.display = "inline-block";
-        topMeme.style.position = "absolute";
-        bottomMeme.style.position = "absolute";
-        topMeme.style.marginBottom = "40px";
-        bottomMeme.style.marginTop = "40px";
-        topMeme.style.color = "white"
-        bottomMeme.style.color = "black";
-        topMeme.style.textAlign = "center";
-        bottomMeme.style.textAlign = "center";
-        topMeme.style.fontSize = "30px";
-        bottomMeme.style.fontSize = "30px";
-        
-        topMeme.style.top = "10%";
-        topMeme.style.left = "50%";
-        topMeme.style.zIndex = "1";
-
-        
-        //prevents refresh after function executes
-        event.preventDefault();
     })
 
 }
 
 
 
-// document.addEventListener("DOMContentLoaded", function(){
-//     const form = document.getElementById("form");
-//     var section = document.querySelector("section")
-//     //when the button with the 'type=submit' is clicked or enter key
-//     //is pressed, the following function will run
-//     form.addEventListener("submit", function(){
-//         var topText = document.querySelector(".top-text").value
-//         //var bottomText = document.querySelector(".bottom-text").vaule
-
-//         // this will alert the input value of the top-text field 
-//         //alert(bottomText);
-//         var test = document.createElement("div");
-//         test.innerText = "working";
-//     //prevents refresh
-//     //event.preventDefault();
-//     });
-//})
